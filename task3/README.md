5372affbe20661474c3edc06c92eb9a03c6893eb
JordanAttys21 <87765742+JordanAttys21@users.noreply.github.com>
2021-08-12 21:01:50 -0400

    first

--- task3/README.md
+++ task3/README.md
@@ -1,4 +1,4 @@
-## Task 3
+1l## Task 3
 
 ## Intructions
 
--- task3/script.js
+++ task3/script.js
@@ -1,1 +1,23 @@
-// Your code goes here:
+let button = document.querySelector("#race");
+let images = document.querySelectorAll("img");
+
+
+let randInt = Math.floor(Math.random() * 5 + 1);
+let randInt2 = Math.floor(Math.random() * 5 + 1);
+
+button.onclick = event => {
+    event.preventDefault();
+   
+    images[0].style.animationName = "Move";
+    images[0].style.animationDuration = randInt + "s";
+    
+
+    images[1].style.animationName = "Move";
+    images[1].style.animationDuration = randInt2 + "s";
+    
+}
+
+
+
+
+
